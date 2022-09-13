3d92300e2504ac02e2ce21d82c7eaf04fda990f7
Esterod215 <estevan.rodriguez215@gmail.com>
2022-09-13 03:51:39 -0700

    Set up search component and functionality.

--- src/components/SearchForm.js
+++ src/components/SearchForm.js
@@ -1,11 +1,37 @@
-import React from 'react';
+import React, { useEffect, useRef } from 'react';
 
 import { useGlobalContext } from '../context';
 
 const SearchForm = () => {
 	const { setSearchTerm } = useGlobalContext();
+	const searchValue = useRef('');
+
+	useEffect(() => {
+		searchValue.current.focus();
+	})
+
+	const searchCocktail = () => {
+		setSearchTerm(searchValue.current.value);
+	}
+
+	const submitSearch = (event) => {
+		event.preventDefault();
+	}
+
 	return (
-		<h1>Search</h1>
+		<section className="section search">
+			<form className="search-form" onSubmit={submitSearch}>
+				<div className="form-control">
+					<label htmlFor="name">Search Cocktails</label>
+					<input
+					id="name"
+					type="text"
+					ref={searchValue}
+					onChange={searchCocktail}
+					/>
+				</div>
+			</form>
+		</section>
 	)
 }
 
